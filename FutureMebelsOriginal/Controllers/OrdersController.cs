7c071e59a2ace05dab2a3501f77b6851c9675b23
Никола Николов <nn18153227@edu.mon.bg>
2023-05-26 17:54:01 +0300

    saffa

--- FutureMebelsOriginal/Controllers/OrdersController.cs
+++ FutureMebelsOriginal/Controllers/OrdersController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace FutureMebelsOriginal.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    
     public class OrdersController : Controller
     {
       
